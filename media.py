94d652f6b3f9e5d140cbd76efd3ed30339378b69
10eputzen <stef.development@gmail.com>
2017-08-06 19:52:28 +0200

    Format Media.py

--- media.py
+++ media.py
@@ -1,9 +1,11 @@
 #  The Movie Base Class
 class Movie():
-	#  Movie class stores title, year, storyline, image url and trailer url of a provided movie
+    # Movie class stores title, year, storyline, image url and trailer url of
+    # a provided movie
+
     def __init__(self, movie_title, movie_year, movie_storyline, poster_image,
                  trailer_youtube):
-    	#  set the Movie class variables to the input parameter
+        #  set the Movie class variables to the input parameter
         self.title = movie_title
         self.year = movie_year
         self.storyline = movie_storyline
